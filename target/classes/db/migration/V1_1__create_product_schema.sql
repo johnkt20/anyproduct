5fff62164bed19627ac4d7a69c36a3d80af26421
Yohannes <johnkelati16@gmail.com>
2022-07-12 16:06:28 -0500

    delete

--- target/classes/db/migration/V1_1__create_product_schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists product;
-CREATE TABLE PRODUCT (
-                                    id bigint(20) NOT NULL AUTO_INCREMENT,
-                                    name varchar(100) NOT NULL,
-                                    price varchar(50) NOT NULL,
-                                    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
